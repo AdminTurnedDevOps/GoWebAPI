9f998bae93e0f5b2c4897333bea049b4125f4f60
Michael Levan <mlevan1992@gmail.com>
2020-08-23 19:53:48 -0400

    Sample web API

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	request1()
+}
+
+func homePage(response http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Welcome to the Go Web API!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func aboutMe(response http.ResponseWriter, r *http.Request) {
+	who := "MichaelLevan"
+
+	fmt.Fprintf(response, "A little bit about Michael Levan...")
+	fmt.Println("Endpoint Hit: ", who)
+}
+
+func request1() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/aboutme", aboutMe)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
