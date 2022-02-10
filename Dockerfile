a8dd69fa5dd02f2fd5530a72e6ecdc41fc698347
Michael Levan <mlevan1992@gmail.com>
2022-02-10 14:27:45 -0500

    main

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,11 @@ RUN mkdir /build
 WORKDIR /build
 
 RUN export GO111MODULE=on 
-RUN go get github.com/AdminTurnedDevOps/GoWebAPI/main
+RUN go get github.com/AdminTurnedDevOps/GoWebAPI/app
 RUN cd /build && git clone https://github.com/AdminTurnedDevOps/GoWebAPI.git
 
 RUN cd /build/GoWebAPI/main && go build
 
 EXPOSE 8080
 
-ENTRYPOINT [ "/build/GoWebAPI/main/main" ]
+ENTRYPOINT [ "/build/GoWebAPI/app/main" ]
---
+++ app/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type whoami struct {
+	Name  string
+	Title string
+	State string
+}
+
+func main() {
+	request1()
+}
+
+func whoAmI(response http.ResponseWriter, r *http.Request) {
+	who := []whoami{
+		whoami{Name: "Michael Levan",
+			Title: "Developer Advocate",
+			State: "NJ",
+		},
+	}
+
+	json.NewEncoder(response).Encode(who)
+
+	fmt.Println("Endpoint Hit", who)
+}
+
+func homePage(response http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(response, "Welcome to the Go Web API!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func aboutMe(response http.ResponseWriter, r *http.Request) {
+	who := "MichaelLevan"
+
+	fmt.Fprintf(response, "A little bit about Michael Levan...")
+	fmt.Println("Endpoint Hit: ", who)
+}
+
+func request1() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/aboutme", aboutMe)
+	http.HandleFunc("/whoami", whoAmI)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
